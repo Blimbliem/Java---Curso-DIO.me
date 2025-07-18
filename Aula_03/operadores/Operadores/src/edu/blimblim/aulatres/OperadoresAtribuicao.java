88cc9bc74a02d903840f9444abaf630c1d1b5cda
Blimbliem <studyblimblim@gmail.com>
2025-07-17 23:19:16 -0300

    Operadores Aritmeticos e de atribuição

---
+++ Aula_03/operadores/Operadores/src/edu/blimblim/aulatres/OperadoresAtribuicao.java
@@ -0,0 +1,13 @@
+package edu.blimblim.aulatres;
+public class OperadoresAtribuicao {
+    public static void main(String[] args) throws Exception {
+        // classe Operadores.java
+        String nome = "Gleyson";
+        int idade = 22;
+        double peso = 81.3;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+       // Date dataNascimento = new Date();
+
+    }
+}
